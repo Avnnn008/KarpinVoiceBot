8810d52dcd9b058f997a90caf62f51ab83983313
Anastasia <ohnnohnn@yandex.ru>
2023-11-09 15:31:16 +0300

    created

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import { Telegraf } from "telegraf";
+import config from 'config'
+import { VOICELIST } from "./voicelist"; 
+
+const bot = new Telegraf(config.get('TELEGRAM_BOT_TOKEN'))
+
+bot.on('inline_query', async ctx => {
+    const result = 
+    VOICELIST.map(el=> ({
+        id: Math.random().toString(),
+        type: 'voice',
+        voice_url: el.url,
+        title: el.name
+    }))
+
+    await ctx.answerInlineQuery(result)
+})
+
+
+
+bot.launch()
+process.once('SIGINT', ()=>bot.stop('SIGINT'))
+process.once('SIGTERM', ()=>bot.stop('SIGTERM'))
---
+++ src/voicelist.js
@@ -0,0 +1,15 @@
+export const VOICELIST = [
+    {name: 'Игра была равна',
+url:'https://storage.yandexcloud.net/tg-voice-bot/OGG/%D0%B8%D0%B3%D1%80%D0%B0%20%D0%B1%D1%8B%D0%BB%D0%B0%20%D1%80%D0%B0%D0%B2%D0%BD%D0%B0.ogg'},
+{name: 'Вы че не видели',
+url:'https://storage.yandexcloud.net/tg-voice-bot/OGG/%D0%B2%D1%8B%20%D1%87%D0%B5%20%D0%BD%D0%B5%20%D0%B2%D0%B8%D0%B4%D0%B5%D0%BB%D0%B8.ogg'},
+{name: 'Интернет не читаю',
+url:'https://storage.yandexcloud.net/tg-voice-bot/OGG/%D0%B8%D0%BD%D1%82%D0%B5%D1%80%D0%BD%D0%B5%D1%82%20%D0%BD%D0%B5%20%D1%87%D0%B8%D1%82%D0%B0%D1%8E.ogg'},
+{name: 'Мысли, эмоции, ощущения',
+url:'https://storage.yandexcloud.net/tg-voice-bot/OGG/%D0%BC%D1%8B%D1%81%D0%BB%D0%B8%20%D1%8D%D0%BC%D0%BE%D1%86%D0%B8%D0%B8.ogg'},
+{name: 'Фалькао к нам не поедет',
+url:'https://storage.yandexcloud.net/tg-voice-bot/OGG/%D1%84%D0%B0%D0%BB%D1%8C%D0%BA%D0%B0%D0%BE%20%D0%BA%20%D0%BD%D0%B0%D0%BC%20%D0%BD%D0%B5%20%D0%BF%D0%BE%D0%B5%D0%B4%D0%B5%D1%82ogg.ogg'},
+{
+    name: 'Ненавижу негров',
+    url: 'https://storage.yandexcloud.net/tg-voice-bot/OGG/%D0%BD%D0%B5%D0%BD%D0%B0%D0%B2%D0%B8%D0%B6%D1%83%20%D0%BD%D0%B5%D0%B3%D1%80%D0%BE%D0%B2.ogg'
+}]
